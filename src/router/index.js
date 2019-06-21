12551c239be64f3e7b295d14af5d4f15ea04070b
jasmine <youjingqiang@vip.qq.com>
2019-06-21 21:43:50 +0800

    merge

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { routes as home } from '$modules/home'
 import { routes as auth } from '$modules/auth'
 import { routes as store } from '$modules/store'
+import { routes as collection } from '$modules/collection'
 import { routes as me } from '$modules/me'
 import Vue from 'vue'
 import Router from 'vue-router'

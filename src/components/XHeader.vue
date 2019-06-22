32852c107802410b8cb8572391d4f7b7da0e85df
JiBingli <1173812432@qq.com>
2019-06-22 18:52:59 +0800

    jibingli

--- src/components/XHeader.vue
+++ src/components/XHeader.vue
@@ -15,10 +15,10 @@
 </template>
 
 <script>
-import NavBar from 'vant/lib/nav-bar'
-import 'vant/lib/nav-bar/style'
+import NavBar from "vant/lib/nav-bar";
+import "vant/lib/nav-bar/style";
 export default {
-  name: 'x-header',
+  name: "x-header",
   components: {
     NavBar
   },
@@ -28,49 +28,91 @@ export default {
     },
     url: {
       type: String,
-      default: ''
+      default: ""
     },
     leftText: {
       type: String,
-      default: ''
+      default: ""
     },
     rightText: {
       type: String,
-      default: ''
+      default: ""
     },
     backColor: {
       type: String,
-      default: '#ffffff'
+      default: "#ffffff"
     },
     color: {
       type: String,
-      default: '#020202'
+      default: "#020202"
     },
     underlineColor: {
       type: String,
-      default: '#ededed'
+      default: "#ededed"
     }
   },
   methods: {
     back() {
-      if (this.url == '') {
-        this.$router.back()
+      if (this.url == "") {
+        this.$router.back();
       } else {
-        this.$router.push({ name: this.url })
+        this.$router.push({ name: this.url });
       }
     },
-    onClickRight () {
-      this.$emit('click-right')
+    onClickRight() {
+      this.$emit("click-right");
     }
   },
   mounted() {
-    window.document.title = this.title
+    window.document.title = this.title;
   }
-}
+};
 </script>
 
 <style lang="scss">
-.van-nav-bar .van-icon{
+$height: 2.5rem;
+header {
+  height: $height;
+  box-sizing: border-box;
+  text-align: center;
+  z-index: 99;
+  width: 100%;
+  position: relative;
+  div.back-icon {
+    width: $height;
+    height: $height;
+    display: inline-block;
+    line-height: $height;
+    text-align: center;
+    position: absolute;
+    box-sizing: border-box;
+    left: 0;
+    i {
+      font-size: 0.8rem;
+    }
+  }
+  h1 {
+    display: inline-block;
+    font-size: 0.768rem;
+    height: 100%;
+    line-height: $height;
+    margin: 0;
+  }
+  .right-box {
+    position: absolute;
+    width: $height;
+    right: 0;
+    top: 0;
+    height: 100%;
+    text-align: center;
+    line-height: $height;
+    font-size: 0.682667rem;
+    .iconfont {
+      font-size: 0.8rem;
+    }
+  }
+}
+.van-nav-bar .van-icon {
   color: #020202;
 }
 </style>

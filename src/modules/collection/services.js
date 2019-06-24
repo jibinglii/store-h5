73de47477135d487a3f69c3c0099f26da001c838
“jelly <20690895@qq.com>
2019-06-24 18:03:12 +0800

    update 我的收藏

--- src/modules/collection/services.js
+++ src/modules/collection/services.js
@@ -7,5 +7,5 @@ export const deleteCollectionGoods = (goodsId) => {
   return http.delete('/api/v1/collect/goods/' + goodsId)
 }
 export const deleteAllCollectionGoods = (goodsList) => {
-  return http.delete('/api/v1/collects/delete', { data: goodsList })
+  return http.delete('/api/v2/collects/delete', { data: goodsList })
 }

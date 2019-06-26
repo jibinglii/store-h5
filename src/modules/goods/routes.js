674bd36d3c2014a4d5e56803173ebbe040cde812
jasmine <youjingqiang@vip.qq.com>
2019-06-26 09:49:12 +0800

    storeid

--- src/modules/goods/routes.js
+++ src/modules/goods/routes.js
@@ -1,46 +1,43 @@
 export default [{
-        path: 'goods/:goods.html',
-        name: 'goods.view',
-        meta: {
-            requiresAuth: true
-        },
-        component: () =>
-            import ('./goods')
-    },
-    {
-        path: 'confirm/:goods.html',
-        name: 'confirm',
-        meta: {
-            requiresAuth: true
-        },
-        component: () =>
-            import ('./confirm')
-    },
-    {
-        path: 'pay/:order.html',
-        name: 'pay.type',
-        meta: {
-            requiresAuth: true
-        },
-        component: () =>
-            import ('./pay')
-    },
-    {
-        path: 'pay-bank/:order.html',
-        name: 'pay.bank',
-        meta: {
-            requiresAuth: true
-        },
-        component: () =>
-            import ('./DCPay')
-    },
-    {
-        path: 'goods/add.html',
-        name: 'goods.add',
-        meta: {
-            requiresAuth: true
-        },
-        component: () =>
-            import ('./add')
-    }
+  path: 'goods/:goods.html',
+  name: 'goods.view',
+  component: () =>
+    import('./goods')
+},
+{
+  path: 'confirm/:goods.html',
+  name: 'confirm',
+  meta: {
+    requiresAuth: true
+  },
+  component: () =>
+    import('./confirm')
+},
+{
+  path: 'pay/:order.html',
+  name: 'pay.type',
+  meta: {
+    requiresAuth: true
+  },
+  component: () =>
+    import('./pay')
+},
+{
+  path: 'pay-bank/:order.html',
+  name: 'pay.bank',
+  meta: {
+    requiresAuth: true
+  },
+  component: () =>
+    import('./DCPay')
+},
+{
+  path: 'goods/add.html',
+  name: 'goods.add',
+  meta: {
+    requiresAuth: true
+  },
+  component: () =>
+    import('./add')
+}
 ]

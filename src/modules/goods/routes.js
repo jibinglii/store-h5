d3faf4433a264c6f9f71cad1a475f8d6702ffbc2
jasmine <youjingqiang@vip.qq.com>
2019-06-27 23:37:38 +0800

    style

--- src/modules/goods/routes.js
+++ src/modules/goods/routes.js
@@ -1,52 +1,55 @@
 export default [{
-        path: 'goods/:goods.html',
-        name: 'goods.view',
-        meta: {
-            requiresAuth: true
-        },
-        component: () =>
-            import ('./goods')
-    },
-    {
-        path: 'confirm/:goods.html',
-        name: 'confirm',
-        meta: {
-            requiresAuth: true
-        },
-        component: () =>
-            import ('./confirm')
-    },
-    {
-        path: 'pay/:order.html',
-        name: 'pay.type',
-        meta: {
-            requiresAuth: true
-        },
-        component: () =>
-            import ('./pay')
-    },
-    {
-        path: 'pay-bank/:order.html',
-        name: 'pay.bank',
-        meta: {
-            requiresAuth: true
-        },
-        component: () =>
-            import ('./DCPay')
-    },
-    {
-        path: 'goods/add',
-        name: 'goods.add',
-        component: () =>
-            import ('./add')
-    },
-    {
-        path: 'goods/goodsort',
-        name: 'goods.goodsort',
-        meta: {
-            requiresAuth: true
-        },
-        component: () =>
-            import ('./goodSort')
-    }
+  path: 'goods/:goods.html',
+  name: 'goods.view',
+  meta: {
+    requiresAuth: true
+  },
+  component: () =>
+    import('./goods')
+},
+{
+  path: 'confirm/:goods.html',
+  name: 'confirm',
+  meta: {
+    requiresAuth: true
+  },
+  component: () =>
+    import('./confirm')
+},
+{
+  path: 'pay/:order.html',
+  name: 'pay.type',
+  meta: {
+    requiresAuth: true
+  },
+  component: () =>
+    import('./pay')
+},
+{
+  path: 'pay-bank/:order.html',
+  name: 'pay.bank',
+  meta: {
+    requiresAuth: true
+  },
+  component: () =>
+    import('./DCPay')
+},
+{
+  path: 'goods/add',
+  name: 'goods.add',
+  meta: {
+    requiresAuth: true
+  },
+  component: () =>
+    import('./add')
+},
+{
+  path: 'goods/goodsort',
+  name: 'goods.goodsort',
+  meta: {
+    requiresAuth: true
+  },
+  component: () =>
+    import('./goodSort')
+}
 ]

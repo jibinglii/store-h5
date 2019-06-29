4d8343b9e876a391847def0f1ca8e1e77e42a368
jasmine <youjingqiang@vip.qq.com>
2019-06-29 14:34:16 +0800

    结算

--- src/modules/withdraw/routes.js
+++ src/modules/withdraw/routes.js
@@ -1,19 +1,38 @@
-export default [{
+export default [
+  {
         path: 'withdraw/withdraw',
         name: 'withdraw.withdraw',
+        meta: {
+          requiresAuth: true
+      },
         component: () =>
             import ('./withdraw')
     },
     {
         path: 'withdraw/paypwd',
         name: 'withdraw.paypwd',
+        meta: {
+          requiresAuth: true
+      },
         component: () =>
             import ('./payPwd')
     },
     {
         path: 'withdraw/resultdetails',
         name: 'withdraw.resultdetails',
+        meta: {
+          requiresAuth: true
+      },
         component: () =>
             import ('./resultDetails')
-    }
+    },
+    {
+      path: 'withdraw/history',
+      name: 'withdraw.history',
+      meta: {
+        requiresAuth: true
+    },
+      component: () =>
+          import ('./history')
+  },
 ]
